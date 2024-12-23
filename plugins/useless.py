2c17d431205d477cbb5900c1b00412db05de2403
Sukuna69x <niloygain29@gmail.com>
2024-12-24 00:24:01 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,8 +1,9 @@
 from bot import Bot
 from pyrogram.types import Message
 from pyrogram import filters
-from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
+from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT, START_PIC
 from datetime import datetime
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from helper_func import get_readable_time
 
 @Bot.on_message(filters.command('stats') & filters.user(ADMINS))
@@ -13,7 +14,30 @@ async def stats(bot: Bot, message: Message):
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
 
-@Bot.on_message(filters.private)
-async def useless(_,message: Message):
+@Bot.on_message(filters.private & filters.incoming)
+async def useless(_, message: Message):
     if USER_REPLY_TEXT:
-        await message.reply(USER_REPLY_TEXT)
+        # Inline keyboard buttons
+        buttons = [
+            [
+                InlineKeyboardButton("Hanime Xoxoxo", url="https://t.me/+3OxTjM8ed2wxMzk1"),
+                InlineKeyboardButton("Cosplay Xoxoxo", url="https://t.me/+PlwsfDABvqZkN2Zl"),
+            ],
+            [
+                InlineKeyboardButton("JAV Xoxoxo", url="https://t.me/+L-KE0ee0R0U2NDRl"),
+                InlineKeyboardButton("Indian Xoxoxo", url="https://t.me/+rI17-d7By7JhMzQ1"),
+            ],
+        ]
+        # Check if START_PIC is set
+        if START_PIC:
+            await message.reply_photo(
+                photo=START_PIC,
+                caption=USER_REPLY_TEXT,
+                reply_markup=InlineKeyboardMarkup(buttons),
+            )
+        else:
+            await message.reply(
+                text=USER_REPLY_TEXT,
+                reply_markup=InlineKeyboardMarkup(buttons),
+            )
+                
